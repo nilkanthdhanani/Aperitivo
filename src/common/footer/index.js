d297caabf31899548df1fd5675755119ecd6c89b
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-09 12:25:37 +0530

    footer completed

--- src/common/footer/index.js
+++ src/common/footer/index.js
@@ -1,30 +1,47 @@
 
+import Link from 'next/link';
 import styles from './footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <p>The Footer</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt officia dolore corrupti recusandae odio esse! Quibusdam hic maxime, quas distinctio fugiat sint, nisi quasi quo cupiditate illum nam quisquam corporis?
-        Tenetur ad quasi accusamus ipsa molestiae explicabo debitis vitae quaerat a consequatur tempore odio eius, esse, aut quos id, doloribus dolore dolorem voluptas sapiente reprehenderit! Ducimus dicta officiis sunt veritatis?
-        Dolorem exercitationem vero dicta maxime quod modi perferendis dolore optio labore vitae? Non pariatur nihil dicta! Qui ratione, eos laudantium, explicabo placeat molestiae repellendus nisi, perferendis molestias quia officiis exercitationem.
-        Porro enim esse sint iste dolores distinctio qui, tempore commodi tenetur quo, voluptate quia? Iusto quos aliquid voluptate ex minima dolorum ut explicabo tenetur deleniti nam placeat aspernatur, cupiditate ab.
-        Ab, rem. Ex eligendi architecto cum quam labore eveniet corrupti, est expedita quidem laudantium quaerat voluptatibus libero a ipsam dolorem placeat nam ipsum delectus harum repellat recusandae modi? Sapiente, facilis?
-        Veniam dolorem corrupti obcaecati ipsum tenetur voluptates? Libero facere inventore alias consequuntur accusamus natus optio sequi harum corrupti maiores minima debitis sint, labore eveniet exercitationem cupiditate atque voluptatem! Et, velit.
-        Maxime cum nesciunt numquam ea autem voluptatum dolorum beatae, quo doloremque inventore suscipit facere quod minima labore temporibus dolore nemo, unde in doloribus iusto deleniti aspernatur. Esse accusantium explicabo perferendis?
-        Natus mollitia veritatis maiores reprehenderit, minus sit provident minima amet similique. Explicabo fugit facere maxime voluptatum autem, nulla assumenda sapiente voluptates reprehenderit impedit debitis voluptatem non nam cum reiciendis dolores.
-        Mollitia doloremque iusto hic impedit qui saepe aspernatur placeat praesentium quos doloribus voluptatem dolorem repellat nulla illo dolor, est vitae repellendus! Unde sunt debitis numquam amet recusandae delectus vero fugit?
-        Dolorum nisi ut eligendi. Nesciunt, error commodi. Sint sed nihil, ea temporibus ad minus laborum quidem minima maxime eius dolore expedita, tempora asperiores quo! Quae dignissimos amet nisi aliquam expedita?
-        Maiores doloremque quam accusantium quis officiis adipisci perspiciatis debitis vero, voluptatibus possimus officia eius laudantium inventore doloribus facilis optio, nemo reiciendis tempora ipsa consequuntur voluptas veniam. Amet, laudantium velit. Dolorum!
-        Ea doloribus pariatur possimus ad porro unde molestiae nam ullam doloremque hic voluptatum maxime eos tenetur voluptas ipsam a minus, quae impedit at sed non, itaque quibusdam consequatur sapiente. Magni?
-        Atque laboriosam voluptas deserunt deleniti non quam doloremque, quos repellat laborum eum facere! A obcaecati ullam sapiente maiores ipsum placeat repudiandae, voluptate eius, omnis accusantium quis sit, dolore porro hic?
-        Animi, totam aliquid! Nemo, provident? Nihil maxime ipsum ipsa sed reprehenderit tenetur nostrum ipsam dolorum, consectetur non minima dicta totam aliquam beatae? Sit vero quam repellendus exercitationem, vel maxime porro?
-        Ut officiis dolorem culpa, aperiam libero perspiciatis impedit repellat quo esse aut autem excepturi quos deserunt, totam neque dicta ducimus hic! Tenetur magni fugiat suscipit mollitia reiciendis, veniam adipisci harum.
-        Deserunt odit, dolorem obcaecati similique voluptates quam a cumque natus commodi quasi. Iusto nesciunt minima qui excepturi et modi perferendis! Incidunt voluptate pariatur, quas repudiandae nihil mollitia sint? Autem, minus.
-        Perferendis, quibusdam? Dolor iusto sint, sunt beatae exercitationem fugiat nulla eum facere porro iste adipisci accusantium laudantium ipsum ab tempora necessitatibus omnis maxime? Dicta modi cumque, pariatur rem harum dolorem!
-        Dolores rerum inventore quos libero quae cupiditate fugit eius facere. Est quisquam esse velit. Quasi quae, numquam a veritatis aut unde qui quis rerum, sunt ex cum aliquid, dolore id.
-        Quisquam laudantium aliquid quos tempore, rem adipisci quaerat, consequuntur tempora provident dolorem praesentium explicabo quasi sit esse eveniet temporibus blanditiis fugit est! Quas, necessitatibus architecto! Unde facilis repudiandae ipsam placeat?
-        Quam pariatur saepe asperiores ducimus repellat debitis dolore rem deleniti nesciunt ullam sunt consequatur voluptas odit similique dignissimos, temporibus, vitae distinctio veniam! Iusto ratione quasi aliquam, illo veritatis omnis corporis!</p>
+      <div className={styles.container}>
+        <div className={styles.footerDiv}>
+          <div className={styles.footerContact}>
+            <h2>Contact</h2>
+            <div className={styles.footerMail}>
+              <Link href={"mailto:info@mysite.com"}>info@mysite.com</Link>
+              <span>123-456-7890</span>
+            </div>
+            <span>500 Terry Francine Street</span>
+            <span>San Francisco, CA 94158</span>
+          </div>
+          <div className={styles.footerLogo}>
+            <div className={styles.logo}>
+              <Link href={"/"}>Apéritivo</Link>
+            </div>
+            <div className={styles.footerLink}>
+              <Link href={"https://www.facebook.com/"} target='_blank'>Facebook</Link>
+              <div className={styles.footerLinkDot}></div>
+              <Link href={"https://x.com/"} target='_blank'>Twitter</Link>
+              <div className={styles.footerLinkDot}></div>
+              <Link href={"https://www.instagram.com/"} target='_blank'>Instagram</Link>
+            </div>
+          </div>
+          <div className={styles.footerContact}>
+            <h2>Service</h2>
+            <div className={styles.footerMail}>
+              <span>Same-Day delivery</span>
+              <span>All over San Francisco</span>
+            </div>
+            <span>Mon - Fri: 11am - 10pm</span>
+            <span>Sat - Sun: 11am - 12am</span>
+          </div>
+        </div>
+        <div className={styles.copyRight}>
+          <p>©2035 by Apéritivo. All rights reserved.</p>
+        </div>
+      </div>
     </footer>
   )
 }
