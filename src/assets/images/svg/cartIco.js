96ea2d43d045ef26c67b63eadac81940b1c2297d
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-11 14:42:47 +0530

    seo increse

--- src/assets/images/svg/cartIco.js
+++ src/assets/images/svg/cartIco.js
@@ -1,7 +1,7 @@
 
 
 const CartIco = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="auto" height="25px" viewBox="0 0 164.9 196.4" preserveAspectRatio="xMinYMax meet" data-hook="svg-icon-9"><path d="M81.9 11.5c-18.8 0-34.1 16-34.1 35.7v18.1h7.8V47.2c0-15.4 11.8-27.9 26.4-27.9 14.5 0 26.4 12.5 26.4 27.9v18.1h6.6V64h1.1V47.2c-.1-19.7-15.4-35.7-34.2-35.7z"></path><path d="M156.9 70.5v118H8v-118h148.9m8-8H0v134h164.9v-134z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="25px" viewBox="0 0 164.9 196.4" preserveAspectRatio="xMinYMax meet" data-hook="svg-icon-9"><path d="M81.9 11.5c-18.8 0-34.1 16-34.1 35.7v18.1h7.8V47.2c0-15.4 11.8-27.9 26.4-27.9 14.5 0 26.4 12.5 26.4 27.9v18.1h6.6V64h1.1V47.2c-.1-19.7-15.4-35.7-34.2-35.7z"></path><path d="M156.9 70.5v118H8v-118h148.9m8-8H0v134h164.9v-134z"></path></svg>
 );
 
 export default CartIco;
