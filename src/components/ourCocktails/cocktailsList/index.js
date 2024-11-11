29e775337177de6a8c39ecc9a545e4fd28368b8f
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-11 16:07:38 +0530

    our cocktails page completed

--- src/components/ourCocktails/cocktailsList/index.js
+++ src/components/ourCocktails/cocktailsList/index.js
@@ -3,44 +3,122 @@ import styles from './cocktailsList.module.scss'
 
 import ourCocktailBg from "../../../assets/images/webp/ourCocktailBg.webp"
 
+const cocktails = [
+    {
+        title: 'Oldies Remix',
+        items: [
+            {
+                name: 'Bloody Cherry',
+                description: 'A bold blend of cherry and vodka with a hint of spice.',
+                price: '$10.50',
+            },
+            {
+                name: 'Magic Hour',
+                description: 'A tropical delight with mango, pineapple, and a splash of rum.',
+                price: '$11.00',
+            },
+            {
+                name: 'Tiki Taka',
+                description: 'A refreshing cocktail with lime, coconut, and a touch of tequila.',
+                price: '$10.75',
+            },
+            {
+                name: 'Americana',
+                description: 'A classic mix of bourbon and bitters with a twist of orange.',
+                price: '$12.00',
+            },
+        ],
+    },
+    {
+        title: 'Bitter Galore',
+        items: [
+            {
+                name: 'Pomelo Yuzu',
+                description: 'A tangy, citrus blend with pomelo and yuzu notes.',
+                price: '$10.25',
+            },
+            {
+                name: 'Lime Supreme',
+                description: 'An intense lime cocktail with gin and a hint of mint.',
+                price: '$10.75',
+            },
+            {
+                name: 'Ginger White',
+                description: 'A zesty mix of ginger, lime, and gin with a smooth finish.',
+                price: '$11.25',
+            },
+            {
+                name: 'Citrus Blend',
+                description: 'A refreshing blend of orange, lemon, and a hint of bitters.',
+                price: '$10.50',
+            },
+        ],
+    },
+    {
+        title: 'Floral Fusion',
+        items: [
+            {
+                name: 'Lavender Dew',
+                description: 'A soothing mix of lavender, lemon, and gin.',
+                price: '$10.75',
+            },
+            {
+                name: 'Lilly Rose',
+                description: 'A floral cocktail with rose water and vodka.',
+                price: '$11.00',
+            },
+            {
+                name: 'Jasmin Edge',
+                description: 'A delicate blend of jasmine tea and rum with a hint of lemon.',
+                price: '$11.50',
+            },
+            {
+                name: 'Spicy Blossom',
+                description: 'A unique mix of hibiscus and spice-infused tequila.',
+                price: '$12.25',
+            },
+        ],
+    },
+    {
+        title: 'Apéritivo Box',
+        items: [
+            {
+                name: 'Triple Ace',
+                description: 'A crisp and bitter aperitivo with a hint of grapefruit.',
+                price: '$10.00',
+            },
+            {
+                name: 'Sunset Kit',
+                description: 'A smooth blend of prosecco and peach with a splash of lime.',
+                price: '$10.50',
+            },
+        ],
+    },
+];
+
+
 export default function CocktailsList() {
-  return (
-    <section className={styles.cocktailsList}>
-      <div className={styles.cocktailsListImg}>
-        <Image src={ourCocktailBg} alt="Cocktail" />
-      </div>
-      <div className={styles.cocktailsListContent}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestias expedita et? Delectus ab alias quae amet ipsam eveniet, cumque repudiandae commodi sed et ullam consequuntur est maxime voluptatum repellat.
-        Eos exercitationem voluptate officia facilis libero! Quasi aliquid culpa veniam, fugiat sit suscipit, magni iure eum repudiandae repellat porro expedita quas fugit aspernatur quae nesciunt laborum nisi! Labore, nisi. Necessitatibus!
-        Quibusdam illo quam sint optio provident obcaecati excepturi, consectetur atque error hic porro, a quaerat recusandae ratione consequuntur assumenda fugit quos similique eos rerum vero est at! Illo, nemo quia?
-        Architecto ducimus voluptas repudiandae hic omnis, a, molestiae at explicabo totam ut accusamus unde. Quibusdam reiciendis nisi nobis facilis nesciunt. Incidunt amet debitis, perferendis beatae omnis impedit dolor possimus minima?
-        Nemo totam cupiditate aspernatur. Pariatur debitis recusandae id nostrum asperiores sequi at quas magni dolorum, cum accusantium velit voluptatibus repudiandae ad voluptates harum similique odit vitae excepturi? Cumque, animi inventore.
-        Eaque doloremque commodi ipsum reiciendis sequi! Magni autem optio ducimus id doloremque vel voluptate maiores consequatur dicta sint dolorem minus ut at, obcaecati, aliquid modi nisi animi dolore magnam assumenda.
-        Officia iure asperiores sint possimus natus unde pariatur atque ipsa quidem! Facere accusantium dolore itaque modi provident omnis fugit placeat molestiae explicabo, alias dolor voluptas ea illum impedit ratione odit.
-        Tempore architecto odio ipsum, aspernatur dolorum necessitatibus impedit consequatur velit eaque illum cupiditate est consectetur molestias, placeat deleniti quos, dignissimos voluptate vel maxime dolore ipsam aliquid id repellendus? Officiis, voluptate?
-        Nobis atque dolores molestias ipsum dolorum recusandae, nihil voluptatem eos, dolorem veritatis culpa, delectus cum! Ipsum consectetur nostrum quam repellendus odio perferendis, quibusdam quo expedita accusamus quidem rerum sed consequatur.
-        Odit ullam harum commodi dolore accusantium necessitatibus ut non maiores natus aspernatur quibusdam deserunt labore facilis, vero cupiditate architecto delectus ipsum sed autem. Tempora maiores similique numquam inventore provident accusantium.
-        Odio, vel a voluptatem, sunt deleniti optio similique maiores accusantium numquam obcaecati quia incidunt recusandae cupiditate autem cumque nulla? Deserunt reprehenderit mollitia temporibus, tempora reiciendis odit iure aut qui? Odit!
-        Quibusdam tempora, sapiente consectetur numquam odio magni quas. Praesentium optio iure dolore accusantium ipsa fuga quia qui culpa sapiente ex voluptatem deleniti, minima, debitis ullam veniam necessitatibus soluta deserunt eaque!
-        Harum, tempora aliquam iusto error consequuntur placeat sit quibusdam repellat. Sequi, totam? Molestias praesentium a, officiis aliquam eos dolores eum quasi dicta debitis quis, in ipsa natus officia illum asperiores!
-        Natus, iure ea aliquam aut numquam modi dignissimos odit, consequatur tenetur praesentium eaque, alias obcaecati dolor quasi! Placeat, repellendus. Maiores est deleniti esse reprehenderit aut ex dolor tempora sequi delectus.
-        Labore harum obcaecati nesciunt accusantium error, quisquam sunt magnam quas, vel, delectus debitis deserunt amet tempore fugiat quos. Corporis iste provident consectetur libero doloribus quod aliquid nobis, cum facere nam.
-        Dolores veritatis sunt libero saepe possimus quasi, consectetur voluptates inventore nobis! Quisquam excepturi, qui nobis quo unde ipsam minus iure repellat nisi magnam accusantium. Fuga quo reprehenderit harum dignissimos tenetur.
-        Ipsum reprehenderit labore earum blanditiis facilis unde, deleniti voluptate perspiciatis, facere voluptates veritatis culpa minus assumenda, dolores soluta! Odio doloremque cum saepe aperiam maiores quo expedita eaque. Tempore, velit dolorum?
-        Error dolore tempore soluta illum minima, sapiente excepturi cum hic debitis maxime expedita? Dicta placeat quaerat tempore hic veniam explicabo nisi commodi? Provident, dolorum sunt saepe minima velit omnis quaerat.
-        Accusamus sunt ipsum neque, eligendi, odit ad rerum sit expedita exercitationem facere, officia incidunt minus fugiat omnis cupiditate numquam. Labore ipsum, molestiae aperiam cum minus dignissimos placeat. Maiores, dolore alias?
-        Cum dignissimos natus ratione incidunt tempore vero repellendus reprehenderit animi dicta ad accusantium est, quod praesentium at obcaecati consequuntur ducimus sit ab quidem? Ratione nostrum rerum impedit repudiandae recusandae blanditiis!
-        Repellat voluptatum nisi accusamus, hic magnam autem officia veniam similique vero iure ea ex ipsum recusandae, quo ullam porro id excepturi eligendi distinctio voluptatibus rerum doloremque laboriosam. Accusantium, officia itaque.
-        Illo possimus saepe corporis voluptatum recusandae. Adipisci, harum? Soluta id itaque porro! Deleniti, laboriosam. Nemo quam sit et eveniet recusandae aperiam necessitatibus sunt rem sapiente dignissimos! Consequuntur, dolore quos? Repellendus.
-        Autem natus suscipit quod soluta repudiandae dolores doloremque beatae similique? Beatae ratione laborum doloribus voluptates quisquam, vitae, atque reprehenderit velit at animi dignissimos veniam possimus saepe. Quibusdam culpa nam atque.
-        Maiores aspernatur ut enim quibusdam velit nostrum quo fugit quae numquam quasi officia, officiis similique iste modi suscipit sint repellendus aliquid accusantium? Inventore, perferendis blanditiis? Aspernatur corrupti at ea pariatur.
-        Molestias at eos, exercitationem quia dicta ipsum veniam praesentium in placeat dolor asperiores inventore animi porro officia earum voluptates accusantium, autem dolore. Accusantium, ullam voluptate natus temporibus optio numquam itaque.
-        Corrupti libero possimus nam fugiat explicabo, expedita nobis officia? Quia hic iure laborum dolorum aperiam, laboriosam inventore commodi dignissimos? Facilis aliquam expedita quibusdam explicabo possimus sed non tenetur, totam corporis!
-        Ea laudantium quas architecto alias tenetur temporibus earum iusto a, sequi incidunt ratione consequuntur provident quos eius quia perspiciatis ipsa quibusdam doloribus corporis hic! Quibusdam officiis quos optio necessitatibus id.
-        Dolorum numquam omnis aut error ab ipsum at corporis, vel eos rerum, quas sunt quasi dolorem asperiores voluptatum velit? Voluptatem, nihil atque molestiae repellat nesciunt dignissimos vero harum optio doloribus.
-        Corporis modi voluptas exercitationem eveniet? Mollitia totam dignissimos, itaque modi ducimus veritatis, cum, optio assumenda eum enim atque facilis corrupti suscipit possimus sapiente minima doloribus ut quod iste? Dolore, fugit?
-        Ratione cumque laborum, reprehenderit voluptatibus alias aut dolore, at voluptas laboriosam quam nulla consectetur recusandae placeat similique iure maxime ad quae ducimus. Molestias odit quod, ipsa eum ipsam earum id!
-      </div>
-    </section>
-  )
+    return (
+        <section className={styles.cocktailsList}>
+            <div className={styles.cocktailsListImg}>
+                <Image src={ourCocktailBg} alt="Cocktail" />
+            </div>
+            <div className={styles.cocktailsListContent}>
+                {cocktails.map((cocktail, index) => (
+                    <div className={styles.cocktailsMain} key={index}>
+                        <h2>{cocktail.title}</h2>
+                        {cocktail.items.map((item, idx) => (
+                            <div className={styles.cocktailGrid} key={idx}>
+                                <div className={styles.cocktailsMainDetail}>
+                                    <h3>{item.name}</h3>
+                                    <p>{item.description}</p>
+                                </div>
+                                <span>{item.price}</span>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
